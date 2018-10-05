04496770e9b66ae8739537d8c164eb78f3110559
EricZH <letItBezhumphries>
2018-10-04 17:55:27 -0700

    "Complete getElementsByClassName"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,28 @@
 //   return document.getElementsByClassName(className);
 // };
 
+//create a storage array
+// var parentNode = document.body;
+//traverse over document nodes
+//check if node classname exists in classList
+// console.log(parentNode);
+//if it does exist push it to storage
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+//check if node has children
+//if it does recursively call our function
+//return the array
+
+// But instead we're going to implement it from scratch:
+var getElementsByClassName = function(className, parentNode = document.body) {
+  var nodeList = [];
+  if (parentNode.classList.contains(className)) {
+    nodeList.push(parentNode);
+  }
+  if (parentNode.hasChildNodes()) {
+    var children = parentNode.children;
+    for (var i = 0; i < children.length; i++) {
+      nodeList = nodeList.concat(getElementsByClassName(className, children[i]));
+    }
+  }
+  return nodeList;
 };
